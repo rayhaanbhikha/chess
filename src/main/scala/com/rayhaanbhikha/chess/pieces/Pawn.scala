d0fb90b893c717418609d73f241968894d54e515
Ray <rayhaan.bhikha@sky.uk>
2018-12-28 14:43:37 +0000

    pawn class refactored

--- src/main/scala/com/rayhaanbhikha/chess/pieces/Pawn.scala
+++ src/main/scala/com/rayhaanbhikha/chess/pieces/Pawn.scala
@@ -3,67 +3,81 @@ package com.rayhaanbhikha.chess.pieces
 import com.rayhaanbhikha.chess.board.ChessBoardSquare
 import com.rayhaanbhikha.chess.services.{AvailableMove, Translation}
 
+import scala.util.control.Breaks.{break, breakable}
+
 case class Pawn(override val name: String, override var currentPosition: String) extends ChessPiece {
 
   override val value: Int = 1
-  var basicMoves: () => List[Translation] = () => List(Translation(0,1), Translation(1,1), Translation(-1,1))
 
-  def getMoves: List[Translation] = {
-    if(active)
-      basicMoves()
-    else
-      Translation(0,2) :: basicMoves()
-  }
+  def possibleMoves: Map[String, List[Translation]] = {
+    var mNorthEast: List[Translation] = List()
+    var mNorth: List[Translation] = List()
+    var mNorthWest: List[Translation] = List()
 
-  def possibleMoves: List[Translation] = color match {
-      case "white" => getMoves
-      case "black" => getMoves.map(Translation.flipSign)
+    color match {
+      case "white" =>
+        mNorth = if(!active) List(Translation(0,1), Translation(0, 2)) ::: mNorth else Translation(0,1) :: mNorth
+        mNorthEast = Translation(1, 1) :: mNorthEast
+        mNorthWest = Translation(-1, 1) :: mNorthWest
+      case "black" =>
+        mNorth = if(!active) List(Translation(0,-1), Translation(0, -2)) ::: mNorth else Translation(0, -1) :: mNorth
+        mNorthEast = Translation(-1, -1) :: mNorthEast
+        mNorthWest = Translation(1, -1) :: mNorthWest
+    }
+    Map(
+      "mNorthEast" -> mNorthEast,
+      "mNorth" -> mNorth,
+      "mNorthWest" -> mNorthWest
+    )
   }
 
   override def getAvailableMoves(chessBoardSquares: Map[String, ChessBoardSquare]): List[String] = {
-    // transform possible moves into actual moves.
-    val availableMoves: List[AvailableMove] = Translation.convertTranslations(possibleMoves.sortWith(_.y < _.y), currentPosition)
-    // verify each possible move.
-    filterAvailableMoves(availableMoves, chessBoardSquares)
+
+    var returnMoves: List[String] = List()
+
+    for((direction, moves) <- possibleMoves) {
+      val availableMoves: List[AvailableMove] = Translation.convertTranslations(moves, currentPosition)
+      direction match {
+        case "mNorthEast" | "mNorthWest" =>
+          returnMoves = filterDiagonal(availableMoves, chessBoardSquares) ::: returnMoves
+        case "mNorth" =>
+          returnMoves = filterVertical(availableMoves, chessBoardSquares) ::: returnMoves
+      }
+    }
+
+    returnMoves
   }
 
-  def filterAvailableMoves(availableMoves: List[AvailableMove], chessBoardSquares: Map[String, ChessBoardSquare]): List[String] = {
-    val sortedAvailableMoves = AvailableMove.sort(color, availableMoves) // sort translation vectors depending on color of pawn.
 
-    val diagonalMoves = sortedAvailableMoves
-      .filter(_.translation.isDiagonal())
-      .flatMap(diagonalCheck(_, chessBoardSquares))
+  def filterVertical(availableMoves: List[AvailableMove], chessBoardSquares: Map[String, ChessBoardSquare]): List[String] = {
+    var filteredMoves: List[String] = List()
 
-    val verticalMoves = sortedAvailableMoves
-      .filter(_.translation.isVertical())
-      .takeWhile(move => chessBoardSquares(move.position).isEmpty) // check if empty
-      .flatMap(verticalCheck(_, chessBoardSquares))
+    breakable {
+      for(move <- availableMoves) {
+        val position = move.position
+        val chessBoardSquare = chessBoardSquares(position)
 
-    diagonalMoves ::: verticalMoves
+        if (!chessBoardSquare.isEmpty)
+          break
+        else
+          filteredMoves = position :: filteredMoves
+      }
+    }
+    filteredMoves
   }
 
-  def diagonalCheck(availableMove: AvailableMove, chessBoardSquares: Map[String, ChessBoardSquare]): Option[String] = {
-
+  def filterDiagonal(availableMoves: List[AvailableMove], chessBoardSquares: Map[String, ChessBoardSquare]): List[String] = {
+    // pawn diagonal moves are only ever 1.
+    val availableMove = availableMoves.head
     val position = availableMove.position
     val chessBoardSquare = chessBoardSquares(position)
+
     if(
       !chessBoardSquare.isEmpty &&
-      chessBoardSquare.chessPiece.color != color // not same color
+        chessBoardSquare.chessPiece.color != color // not same color
     )
-      Some(position)
+      List(availableMove.position)
     else
-      None
-  }
-
-  def verticalCheck(availableMove: AvailableMove, chessBoardSquares: Map[String, ChessBoardSquare]): Option[String] = {
-
-    val position = availableMove.position
-    val chessBoardSquare = chessBoardSquares(position)
-
-    if(chessBoardSquare.isEmpty) {
-      Some(position)
-    } else {
-      None
-    }
+      List()
   }
 }
